77324b4c45fc363272d1e6a7f7053a8826eafc6e
tn <t.noguchi.23.4.890@4nonome.com>
2024-08-04 15:05:14 +0900

    signup~login

--- myapp/settings.py
+++ myapp/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "registration.apps.RegistrationConfig",
+    "comuse.apps.ComuseConfig",
+    "welcome.apps.WelcomeConfig",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = "myapp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
